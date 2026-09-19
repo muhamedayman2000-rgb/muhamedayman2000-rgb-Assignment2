752f2767893a22365e9437fc2f1225190735deb8
Mohamed Ayman <muhamedayman2000@gmail.com>
2026-09-19 08:11:40 +0300

    fix: handle division by zero

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -20,7 +20,9 @@ namespace Calculator
         }
         static public double Divide(double a, double b)
         {
-        
+            if(b == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+            
             return a / b;
         }
     }
