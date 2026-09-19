5004340dd15bedad41fd6013d541cc443f607e22
Mohamed Ayman <muhamedayman2000@gmail.com>
2026-09-19 07:59:22 +0300

    feat: initialize calculator project

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    static public class Calculator
+    {
+        static public double Add(double a, double b)
+        {
+            return a + b;
+        }   
+        static public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        static public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        static public double Divide(double a, double b)
+        {
+        
+            return a / b;
+        }
+    }
+}
